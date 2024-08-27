1ce1debe39a3555a050582d01c320d737b654fb2
Tommaso Ramella <tommaso.ramella90@gmail.com>
2024-08-27 15:42:47 +0200

    Remove cached and poetry from origin

--- backend/cv/validators.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-
-def validate_file_size(file):
-    filesize = file.size
-    max_size = 5000000
-
-    if filesize > max_size:
-        raise ValidationError(f"File size should not exceed {max_size}")
