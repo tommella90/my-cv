e6df7d77b44244e952ac1100d8779121fba5aec2
Tommaso Ramella <tommaso.ramella90@gmail.com>
2024-08-29 22:58:24 +0200

    Add fixiture db

--- backend/core/settings/prod.py
+++ backend/core/settings/prod.py
@@ -5,7 +5,6 @@ import dj_database_url
 DEBUG = False
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
-print(SECRET_KEY)
 
 # ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "tommella-cv-prod-ee14d9b8bb74.herokuapp"]
 ALLOWED_HOSTS = ['tommella-cv-prod-ee14d9b8bb74.herokuapp.com']
@@ -14,7 +13,6 @@ ALLOWED_HOSTS = ['tommella-cv-prod-ee14d9b8bb74.herokuapp.com']
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
-print(DATABASES)
 
 REDIS_URL = os.environ["REDIS_URL"]
 CELERY_BROKER_URL = REDIS_URL
