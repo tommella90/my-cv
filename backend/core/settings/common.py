deeeb8bfd3e4e79c5c7f0638fca62b8af0b49e59
Tommaso Ramella <tommaso.ramella90@gmail.com>
2024-08-30 20:35:08 +0200

    Add githubpages CORS

--- backend/core/settings/common.py
+++ backend/core/settings/common.py
@@ -39,8 +39,23 @@ MIDDLEWARE = [
 
 
 CORS_ALLOWED_ORIGINS = [
+    "https://tommella90.github.io/tommella-frontend/",
     "http://localhost:8080", 
     "http://localhost:8081",
+]
+
+CORS_ALLOW_METHODS = [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+]
+
+CORS_ALLOW_HEADERS = [
+    "Authorization",
+    "Content-Type",
 ]
 
 
